f7e7ce7444e3e4abdee8a727bca29fd2c7b0bf85
guoyan <949038109@qq.com>
2022-09-18 16:53:33 +0800

    修改了端口

--- reggie_take_out/src/test/java/com/gy/reggie/test.java
+++ reggie_take_out/src/test/java/com/gy/reggie/test.java
@@ -3,11 +3,18 @@ package com.gy.reggie;
 import com.gy.reggie.Utils.ValidateCodeUtils;
 import com.gy.reggie.controller.UserController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.servlet.server.Session;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 public class test {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Test
     public void t1(){
         String password = "123456";
@@ -26,5 +33,9 @@ public class test {
         UserController userController;
         Session session = new Session();
         Session.Cookie cookie = session.getCookie();
+    }
+    @Test
+    public void t4(){
+        redisTemplate.opsForValue().set("key1","aa",10l, TimeUnit.SECONDS);
     }
 }
